5e6aaa73043031c2f844094c5a06ee2a781e932b
Ahnaf <ahnaftahmid627@gmail.com>
2020-07-20 21:56:50 +0600

    Implement cheese detail page

--- everycheese/cheeses/views.py
+++ everycheese/cheeses/views.py
@@ -4,3 +4,6 @@ from django.views.generic import ListView, DetailView
 from .models import Cheese
 class CheeseListView(ListView):
     model = Cheese
+
+class CheeseDetailView(DetailView):
+    model = Cheese
